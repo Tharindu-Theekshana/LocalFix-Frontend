d2d3e777d44efdcb0ac8518e87e14717c751503f
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-14 11:59:23 +0530

    implemented page paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 import './App.css'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
     <>
-    
+    <Router>
+        <Routes>
+          <Route path='*' element={<NotFound/>}/>
+          <Route index element={<Home/>}/>
+        </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='text-center flex justify-center items-center h-screen  text-[80px] font-bold bg-blue-50'>
+      <h1>Page Not Found!</h1>
+    </div>
+  )
+}
