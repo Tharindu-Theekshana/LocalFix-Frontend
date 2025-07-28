1b95297ea219d5a68d3052cb6f1d08d9b2ac04a8
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-28 14:20:17 +0530

    updated

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -37,21 +37,17 @@ export default function FooterSection() {
   return (
    <footer className="bg-blue-900 text-white px-6 py-12">
       <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between gap-12">
-        {/* Left: Logo + Text + Social */}
         <div className="sm:w-1/2 space-y-6">
-          {/* Logo */}
           <div className="flex items-center gap-3 justify-center sm:justify-start">
             <img src='src/imgs/LocalFixLOGO[1].png' alt="LocalFix Logo" className="w-20 rounded-full p-1" />
             <span className="text-3xl font-semibold">LocalFix</span>
           </div>
 
-          {/* Description */}
           <p className="text-gray-100 text-[18px] text-center sm:text-left">
             Connecting skilled professionals with customers across Sri Lanka. <br />
             Your trusted platform for quality home and business services.
           </p>
 
-          {/* Social Media Icons */}
 <div className="flex gap-3 justify-center sm:justify-start pt-8">
   <a
     href="#"
@@ -85,9 +81,7 @@ export default function FooterSection() {
 
         </div>
 
-        {/* Right: Link Groups */}
         <div className="grid grid-cols-1 md:grid-cols-3 md:gap-15 gap-5 text-[17px] ">
-          {/* Popular Services */}
           <div>
             <h3 className="font-semibold md:mb-3 mb-1 md:text-[20px] text-lg">Popular Services</h3>
             {popularServices.map((item) => (
@@ -100,7 +94,6 @@ export default function FooterSection() {
           </div>
 
 
-          {/* Get started */}
           <div>
             <h3 className="font-semibold mb-3 text-[18px]">Get started</h3>
             {Getstarted.map((item) => (
@@ -110,7 +103,6 @@ export default function FooterSection() {
             ))}
           </div>
 
-          {/* Quick Links */}
           <div>
             <h3 className="font-semibold mb-3 text-[18px]">Quick Links</h3>
             {QuickLinks.map((item) => (
@@ -122,7 +114,6 @@ export default function FooterSection() {
         </div>
       </div>
 
-      {/* Bottom */}
       <div className="border-t border-white/30 mt-20 pt-4 text-center text-1*1 text-white/90">
         © 2025 LocalFix. All rights reserved.
       </div>
